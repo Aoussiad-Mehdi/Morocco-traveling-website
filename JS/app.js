6b80bee7247044bbc60979c79f97d065f706732d
Mehdi Aoussiad <mehdiouss315@gmail.com>
2020-08-18 17:50:21 +0100

    responsiveness

--- JS/app.js
+++ JS/app.js
@@ -15,13 +15,37 @@ function dropdown1(){
     dropBtn.style.display = 'block';
     dropBtn1.style.display = 'block';
     dropBtn2.style.display = 'block';
-}
+};
 // This function is for the dropdown link (places).
 function dropdown2(){
     content.style.background = 'transparent';
     dropBtn.style.display = 'none';
     dropBtn1.style.display = 'none';
     dropBtn2.style.display = 'none';
-}
+};
+// navbar in mobile view.
+const navbar = document.querySelector('nav');
+const togle = document.querySelector('.toggle button');
+const icon = document.querySelector('.toggle button i');
+togle.addEventListener('click', function(){
+    navbar.classList.toggle('actif');
+    navbar.style.top = '80px';
+    if(navbar.classList != 'navbar'){
+        icon.className = 'fas fa-times';
+    }
+    else{
+        icon.className = 'fas fa-bars';
+    }
+});
+
+// Changing header background on scroll.
+const header = document.querySelector('header');
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 100){
+        header.classList.add('sticky');
+    }else{
+        header.classList.remove('sticky');
+    }
+});
 
 

93fdf2d7f49f1a6882ac98089fcd89cf35c57908
Mehdi Aoussiad <mehdiouss315@gmail.com>
2020-08-18 19:46:19 +0100

    fixing bugs

--- JS/app.js
+++ JS/app.js
@@ -6,12 +6,15 @@ const dropBtn2 = document.querySelector('.drop-btn2');
 /* I tried to set display none for the dropdown content in css but i didn't know why
 it doesn't work, So i did here in Javascript. */
 content.style.background = 'transparent';
+content.style.border = 'none';
 dropBtn.style.display = 'none';
 dropBtn1.style.display = 'none';
 dropBtn2.style.display = 'none';
+
 // This function is for the dropdown link (places).
 function dropdown1(){
     content.style.background = '#fff';
+    content.style.border = '1px solid #000';
     dropBtn.style.display = 'block';
     dropBtn1.style.display = 'block';
     dropBtn2.style.display = 'block';
@@ -19,6 +22,7 @@ function dropdown1(){
 // This function is for the dropdown link (places).
 function dropdown2(){
     content.style.background = 'transparent';
+    content.style.border = 'none';
     dropBtn.style.display = 'none';
     dropBtn1.style.display = 'none';
     dropBtn2.style.display = 'none';

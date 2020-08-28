4350bda91d06bdd1f507f9381e57010bdc89c81d
Aoussiad-Mehdi <mehdiouss315@gmail.com>
2020-08-28 18:31:32 +0100

    animated counter

--- JS/animation.js
+++ JS/animation.js
@@ -8,4 +8,31 @@ window.addEventListener('scroll',function(){
     else{
         cursort.style.display= 'block';
     }
-})
+});
+// The animated counter .
+const counters = document.querySelectorAll('.counter');
+const speed = 250;
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = Math.ceil(count + inc);
+			// Call function every ms
+			setTimeout(updateCount, 40);
+		} else {
+			counter.innerText = target;
+		}
+    }
+    updateCount();
+});
